d629e0bdb25a4352a5b3727c67f7586061834512
Guillermo Aristi <g.zamorano.aristi@icloud.com>
2024-09-10 14:17:12 +0100

    Implement AppWrite User Auths

--- app/(auth)/sign-in/sign-in.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const signIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
-
-export default signIn;
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+export const signIn = async () => {
+    try {
+        // Mutation / Database / Make a Fetch
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        // Create a User Account
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
